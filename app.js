fe59619ea88d94833d179185a3d2262313e86e48
yoonhanseok <74763663+yoonhanseok@users.noreply.github.com>
2022-10-04 11:24:52 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -21,11 +21,18 @@ const app = new Vue({
       for (var i = 0; i < RANDOM10; i++) {
         var DATA01 = Math.ceil((Math.random() * 100))
         var DATA02 = DATA01 * 2
-        var DATAArr = { DATAa: DATA01, DATAb: DATA02 }
+        if(DATA01 % 2 === 0) {
+          var DATA03 = "evenBlue"
+        } else {
+          var DATA03 = ""
+        }
+        var DATAArr = { DATAa: DATA01, DATAb: DATA02, evenOdd: DATA03 }
         this.tableData.push(DATAArr)
+        
 
         console.log(DATA01)
         console.log(DATA02)
+        console.log(DATA03)
         console.log(DATAArr)
       }
     }
